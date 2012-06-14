48dbc587aa85f7ffbb82508114b8f2b59fcbc1fe
Dave Poirier <instinc@gmail.com>
2012-06-14 09:09:36 -0400

    Initial Commit

---
+++ Base32/MF_AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MF_AppDelegate.h
+//  Base32
+//
+//  Created by Dave Poirier on 12-06-14.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MF_AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Base32/MF_AppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  MF_AppDelegate.m
+//  Base32
+//
+//  Created by Dave Poirier on 12-06-14.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "MF_AppDelegate.h"
+
+@implementation MF_AppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
