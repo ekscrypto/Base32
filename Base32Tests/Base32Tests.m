f164eb93dd97e29ac0235999008122e0180d76ac
Matt Rubin <matt@kavaanu.com>
2015-01-25 02:13:05 -0800

    Clean up tests

--- Base32Tests/Base32Tests.m
+++ Base32Tests/Base32Tests.m
@@ -6,7 +6,7 @@
 //  Copyright (c) 2015 Freshcode. All rights reserved.
 //
 
-#import <XCTest/XCTest.h>
+@import XCTest;
 @import Base32;
 
 
@@ -18,31 +18,22 @@
 
 - (void)testRandomData
 {
-    int totalTests = 0;
-    int totalSuccess = 0;
-    int totalFailed = 0;
-    for( int i = 0; i < 300; i++ ) {
+    for (int i = 0; i < 300; i++) {
         NSData *randomDataBlock = [self randomDataBlock];
         NSString *base32Rep = [randomDataBlock base32String];
-        NSData *revertedDataBlock = [MF_Base32Codec dataFromBase32String:base32Rep];
-        if( [randomDataBlock isEqualToData:revertedDataBlock] ) {
-            NSLog(@"SUCCESS: %@", base32Rep);
-            totalSuccess++;
-        } else {
-            XCTFail(@"FAILED: %@\noriginal data block: %@\n reverted data block: %@", base32Rep, randomDataBlock, revertedDataBlock);
-            totalFailed++;
-        }
-        totalTests++;
+        NSData *revertedDataBlock = [NSData dataWithBase32String:base32Rep];
+        XCTAssertTrue([randomDataBlock isEqualToData:revertedDataBlock],
+                      @"%@\n    original data block: %@\n    reverted data block: %@",
+                      base32Rep, randomDataBlock, revertedDataBlock);
     }
-    NSLog(@"Tests completed with %i failures, %i success out of %i tests", totalFailed, totalSuccess, totalTests);
 }
 
--(NSData *)randomDataBlock
+- (NSData *)randomDataBlock
 {
 #define MaxRandomDataLength 64
     unsigned char bytes[MaxRandomDataLength];
     long dataLength = random() % 64;
-    for(int i = 0; i < dataLength; i++ ) {
+    for (int i = 0; i < dataLength; i++) {
         bytes[i] = random() % 256;
     }
 
