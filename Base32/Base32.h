130a79d7a7632154efd010a84e40e32e42461f7e
Matt Rubin <matt@kavaanu.com>
2015-01-25 01:44:48 -0800

    Add iOS framework target

---
+++ Base32/Base32.h
@@ -0,0 +1,19 @@
+//
+//  Base32.h
+//  Base32
+//
+//  Created by Matt Rubin on 1/25/15.
+//  Copyright (c) 2015 Freshcode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Base32.
+FOUNDATION_EXPORT double Base32VersionNumber;
+
+//! Project version string for Base32.
+FOUNDATION_EXPORT const unsigned char Base32VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Base32/PublicHeader.h>
+
+
---
+++ Base32Tests/Base32Tests.m
@@ -0,0 +1,40 @@
+//
+//  Base32Tests.m
+//  Base32Tests
+//
+//  Created by Matt Rubin on 1/25/15.
+//  Copyright (c) 2015 Freshcode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Base32Tests : XCTestCase
+
+@end
+
+@implementation Base32Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
