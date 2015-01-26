d5574cea378863e2b853187c0b91bd3f5a6e6cea
Matt Rubin <matt@kavaanu.com>
2015-01-25 21:49:32 -0800

    Add tests from the RFC

--- Base32Tests/Base32Tests.m
+++ Base32Tests/Base32Tests.m
@@ -16,6 +16,65 @@
 
 @implementation Base32Tests
 
+// The values in this test are found in Section 10 of the Base32 RFC
+// https://tools.ietf.org/html/rfc4648#section-10
+- (void)testRFCValues
+{
+    NSDictionary *vectors = @{@"":       @"",
+                              @"f":      @"MY======",
+                              @"fo":     @"MZXQ====",
+                              @"foo":    @"MZXW6===",
+                              @"foob":   @"MZXW6YQ=",
+                              @"fooba":  @"MZXW6YTB",
+                              @"foobar": @"MZXW6YTBOI======"};
+    [self _testEncodingWithVectors:vectors];
+    [self _testDecodingWithVectors:vectors];
+}
+
+- (void)testUnpaddedRFCValues
+{
+    NSDictionary *vectors = @{@"":       @"",
+                              @"f":      @"MY",
+                              @"fo":     @"MZXQ",
+                              @"foo":    @"MZXW6",
+                              @"foob":   @"MZXW6YQ",
+                              @"fooba":  @"MZXW6YTB",
+                              @"foobar": @"MZXW6YTBOI"};
+    [self _testDecodingWithVectors:vectors];
+}
+
+- (void)testUnpaddedLowercaseRFCValues
+{
+    NSDictionary *vectors = @{@"":       @"",
+                              @"f":      @"my",
+                              @"fo":     @"mzxq",
+                              @"foo":    @"mzxw6",
+                              @"foob":   @"mzxw6yq",
+                              @"fooba":  @"mzxw6ytb",
+                              @"foobar": @"mzxw6ytboi"};
+    [self _testDecodingWithVectors:vectors];
+}
+
+- (void)_testEncodingWithVectors:(NSDictionary *)vectors
+{
+    for (NSString *plaintext in vectors) {
+        NSString *ciphertext = vectors[plaintext];
+
+        NSString *encryptedPlaintext = [[plaintext dataUsingEncoding:NSUTF8StringEncoding] base32String];
+        XCTAssertEqualObjects(encryptedPlaintext, ciphertext, @"");
+    }
+}
+
+- (void)_testDecodingWithVectors:(NSDictionary *)vectors
+{
+    for (NSString *plaintext in vectors) {
+        NSString *ciphertext = vectors[plaintext];
+
+        NSString *decryptedCiphertext = [[NSString alloc] initWithData:[NSData dataWithBase32String:ciphertext] encoding:NSUTF8StringEncoding];
+        XCTAssertEqualObjects(decryptedCiphertext, plaintext, @"");
+    }
+}
+
 - (void)testRandomData
 {
     for (int i = 0; i < 300; i++) {
