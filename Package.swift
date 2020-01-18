fc8d5a04d3406010827eecda322837ef79fb052c
bas-d <7903735+bas-d@users.noreply.github.com>
2020-01-18 18:16:50 +0100

    Add SPM support

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "Base32",
+    platforms: [
+        .iOS(.v8),
+        .macOS(.v10_10),
+        .tvOS(.v9),
+        .watchOS(.v2)
+    ],
+    products: [
+        .library(
+            name: "Base32",
+            targets: ["Base32"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "Base32", dependencies: [], path: "Base32", exclude: ["main.m", "MF_AppDelegate.m"], sources: ["MF_Base32Additions.m"])
+    ]
+)
